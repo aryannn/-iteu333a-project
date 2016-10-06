d0c7d4211e02d4a81592e3ff96c82c565fe89f7a
afviray <afviray@T609-08>
2016-10-06 08:46:08 +0800

    initialize variable

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -15,7 +15,7 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x=0;
     }
     
 }
