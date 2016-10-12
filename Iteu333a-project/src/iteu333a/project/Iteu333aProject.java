d23e1f23c30475b22d2318519d8266c8f51ade1b
cbricafort <cbricafort@192.168.56.1>
2016-10-13 07:49:37 +0800

    Added header & comments...

--- Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
+++ Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Project by:
+ * Hanan Abubacar
+ * Arianne Cera
+ * Cyril Ricafort
+ * Aleeza Viray
  */
 package iteu333a.project;
 
-/**
- *
- * @author easalinas
- */
+//Import here...
 public class Iteu333aProject {
 
-    /**
-     * @param args the command line arguments
-     */
+    //Global variable here...
     public static void main(String[] args) {
-        //TESTU
+        //Main codes here...
     }
     
 }
