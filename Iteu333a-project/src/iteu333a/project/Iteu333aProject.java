4fe95110e7a4e95f7fdf89efd4458cf935a0bd08
Hanan Abubacar <hannahn006@gmail.com>
2016-10-13 08:09:19 +0800

    Add File Handling

--- Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
+++ Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
@@ -6,12 +6,38 @@
  */
 package iteu333a.project;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 //Import here...
 public class Iteu333aProject {
 
     //Global variable here...
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException{
+        
         //Main codes here...
+        Scanner scan = new Scanner(new FileReader("code.txt"));
+        PrintWriter writer = new PrintWriter("Test.java", "UTF-8");
+        
+        while (scan.hasNext()){
+            String str = scan.nextLine();
+            //check syntax;
+            
+            writer.println(str);
+        }
+        writer.close();
+        List cmdAndArgs = Arrays.asList("cmd", "/c", "run.bat");
+        File dir = new File(System.getProperty("user.dir"));
+        
+        ProcessBuilder pb = new ProcessBuilder(cmdAndArgs);
+        pb.directory(dir);
+        Process p = pb.start();
+        
     }
-    
 }
