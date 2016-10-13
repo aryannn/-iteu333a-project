e57b0d4c5bfc0570766c70dbbe06f541728b23dd
Hanan Abubacar <hannahn006@gmail.com>
2016-10-13 08:41:42 +0800

    Check semicolon

--- Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
+++ Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
@@ -12,6 +12,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.List;
@@ -24,20 +25,17 @@ public class Iteu333aProject {
         //Main codes here...
         Scanner scan = new Scanner(new FileReader("code.txt"));
         PrintWriter writer = new PrintWriter("Test.java", "UTF-8");
-        
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("int", "double", "float", "String", "char"));
         while (scan.hasNext()){
             String str = scan.nextLine();
             //check syntax;
-            
+            System.out.println(str);
+            if(!str.endsWith(";")){
+                System.out.print(" Syntax Error, no semicolon at the end");
+
             writer.println(str);
         }
         writer.close();
-        List cmdAndArgs = Arrays.asList("cmd", "/c", "run.bat");
-        File dir = new File(System.getProperty("user.dir"));
-        
-        ProcessBuilder pb = new ProcessBuilder(cmdAndArgs);
-        pb.directory(dir);
-        Process p = pb.start();
         
     }
 }
